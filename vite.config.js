5d2c1902fa4b5467ff28c3b5055c677038c09c33
Akhil Donthi <42883610+AkhilDonthi0@users.noreply.github.com>
2025-07-03 11:28:14 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/akhildonthi.github.io/', // <-- Replace with your actual repo name
+  base: '', // <-- Replace with your actual repo name
 });
